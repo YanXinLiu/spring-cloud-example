796f403e4b9555ac8d1a4fc6b8cdfe6f2ac7ed8d
liuyanxin <liuyanxin@jingkunsystem.com>
2021-02-03 17:11:14 +0800

    add: gradle上传nexus仓库配置添加成功

--- spring-cloud-admin/src/main/java/com/yanxin/admin/config/SecurityConfiguration.java
+++ spring-cloud-admin/src/main/java/com/yanxin/admin/config/SecurityConfiguration.java
@@ -9,6 +9,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+/**
+ * @author sa
+ */
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
